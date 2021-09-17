11a1865b8914308161b8c7d7e042c9ddd1c8b96b
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-09-17 17:00:39 -0400

    Java classes recognized all over the project

--- NSALogin/src/rodriguez/Test.java
+++ NSALogin/src/rodriguez/Test.java
@@ -1,7 +1,23 @@
 package rodriguez;
 
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter password: ");
+        String password = sc.nextLine();
+        User user = new User(password);
 
+        //Setting passwords
+        System.out.println("Password: " + user.getPassword());
+        System.out.println("Salt: " + user.getSalt());
+        System.out.println("Hashed Password: " + user.getHashedPassword());
+        try{
+            NSALoginController.hashUserPassword(user);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
