c44cfb6f33b7f77439e5cfd08652b40f20dca606
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-09-17 15:32:08 -0400

    creating constructor, and initializing member data

--- formletter/src/rodriguez/Account.java
+++ formletter/src/rodriguez/Account.java
@@ -1,4 +1,13 @@
 package rodriguez;
 
 public class Account {
+    private String name;
+    private String lastPurchase;
+    private int daysSincePurchase;
+
+    public Account(String name, String lastPurchase, int daysSincePurchase){
+        this.name = name;
+        this.lastPurchase = lastPurchase;
+        this.daysSincePurchase = daysSincePurchase;
+    }
 }
