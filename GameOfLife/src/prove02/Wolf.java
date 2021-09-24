9a23d901abf2c48d344b48ecf7b8648823d80270
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-09-24 15:32:26 -0400

    wolfy is here

---
+++ GameOfLife/src/prove02/Wolf.java
@@ -0,0 +1,36 @@
+package prove02;
+
+import java.awt.*;
+
+public class Wolf extends Creature implements Movable,Aware, Aggressor{
+
+    @Override
+    public void attack(Creature target) {
+
+    }
+
+    @Override
+    public void senseNeighbors(Creature above, Creature below, Creature left, Creature right) {
+
+    }
+
+    @Override
+    Shape getShape() {
+        return null;
+    }
+
+    @Override
+    Color getColor() {
+        return null;
+    }
+
+    @Override
+    Boolean isAlive() {
+        return null;
+    }
+
+    @Override
+    public void move() {
+
+    }
+}
