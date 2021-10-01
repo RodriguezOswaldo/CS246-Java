6a68240a53086e3cc949f34f37eebd32ee56f695
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-10-01 17:04:02 -0400

    got rid of the Main file cause it was causing problems

--- WeatherApp/src/main/java/cs243/WeatherTest.java
+++ WeatherApp/src/main/java/cs243/WeatherTest.java
@@ -1,4 +1,18 @@
 package cs243;
+import com.google.gson.*;
 
 public class WeatherTest {
+    //https://api.openweathermap.org/data/2.5/weather?q=Rexburg&apiKey=8d7178b81fed26ae734b0d4364f47036
+    public static void main(String[] args){
+        String myWeather;
+
+        MyHttpUtility httpUtility = new MyHttpUtility();
+        myWeather = httpUtility.readHTTP("https://api.openweathermap.org/data/2.5/weather?q=Rexburg&apiKey=8d7178b81fed26ae734b0d4364f47036");
+        System.out.println(myWeather);
+    }
+    public static void gsonHttpReturn(String strParse){
+        Gson gson = new Gson();
+        WeatherConditions we = gson.fromJson(strParse, WeatherConditions.class);
+        System.out.println(WeatherConditions.Main);
+    }
 }
